1d8fabcf629d498004386b71831224aaa8be2980
Ferran Galán <ferran.galan@unige.ch>
2021-08-25 11:23:08 +0200

    Update xMotion_P1_V1.ino
    Format

--- Model P1/Firmware/Arduino/xMotion_P1_V1.ino
+++ Model P1/Firmware/Arduino/xMotion_P1_V1.ino
@@ -1,7 +1,7 @@
 // Copyright (C) 2109 University of Geneva
 
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 3 as 
+// it under the terms of the GNU General Public License, Version 3, as 
 // published by the Free Software Foundation.
 
 // This program is distributed in the hope that it will be useful,
